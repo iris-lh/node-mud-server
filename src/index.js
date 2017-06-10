7a435ea81b0e262b24d33760172281415be71706
Isaac Helbling <imhelbling@gmail.com>
2017-06-10 16:32:18 -0400

    clients are now uniquely identifiable

--- src/index.js
+++ src/index.js
@@ -1,43 +1,27 @@
-const net           = require('net');
-const redux         = require('redux')
+const net   = require('net');
+const redux = require('redux')
+const uuid  = require('uuid/v4')
 
-const CommandParser = require('./CommandParser')
-const reducer       = require('./reducer.js')
-const gameTick      = require('./game-tick.js')
+const config   = require('./config.json')
+const Inputs   = require('./Inputs.js')
+const reducer  = require('./reducer.js')
+const gameTick = require('./game-tick.js')
 
-
-
-const port = 4000
 const store = redux.createStore(reducer)
-const Parser = new CommandParser(store)
-const prompt = '>> '
-
-
-
-function getCommand(data, asdf) {
-  const command = data.toString('utf8').slice(0, -2)
-  console.log(`command: "${command}"`);
-
-  Parser.parse(command)
-
-  console.log(store.getState());
-  console.log(asdf)
-  const response = 'state: ' + store.getState().count
-  console.log(`response: "${response}"`);
-
-  // this.write(Buffer.from(response + '\n'))
-  this.write(prompt)
-}
-
-
 
 const server = net.createServer( socket => {
+  socket.id = uuid()
+  store.dispatch({type: 'ADD_SESSION', payload: socket})
+  const inputs = new Inputs(socket, store)
+
   socket.write('\nWelcome to the Telnet server!\n\n');
-  socket.on('data', getCommand, 'asdf')
-  console.log(socket.remoteAddress);
+  socket.write('Username: ')
+  socket.on('data', (data)=>{
+    inputs.getInput(data) 
+  })
 })
 
-server.listen(port);
+server.listen(config.port);
 
 setInterval(()=>{
   gameTick(store)
--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,37 @@
 module.exports = (state = {
-  sessions: [],
+  sessions: {},
   count: 0
 }, action)=> {
   switch (action.type) {
+
     case 'ADD_SESSION':
+      var newSessions = Object.assign({}, state.sessions)
+      newSessions[action.payload.id] = {
+        inputGetter: 'getUsername'
+      }
       return Object.assign({}, state, {
-          sessions: [...state.sessions, action.payload]
+        sessions: newSessions
       });
+
+    case 'CHANGE_INPUT_GETTER':
+      var newSessions = Object.assign({}, state.sessions)
+      newSessions[action.payload.socket.id] = {
+        inputGetter: action.payload.inputGetter
+      }
+      return Object.assign({}, state, {
+        sessions: newSessions
+      });
+
     case 'INC':
       return Object.assign({}, state, {
-          count: state.count + 1
+        count: state.count + 1
       });
+
     case 'DEC':
       return Object.assign({}, state, {
-          count: state.count - 1
+        count: state.count - 1
       });
+
     default:
       return state
   }
