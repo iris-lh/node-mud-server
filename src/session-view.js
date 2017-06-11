acda29e990c55e4f1901d7c8926ae8f2df9cd6fa
Isaac Helbling <imhelbling@gmail.com>
2017-06-11 13:16:16 -0400

    add session-view to manage view contexts

---
+++ src/session-view.js
@@ -0,0 +1,16 @@
+const config = require('./config')
+
+function sessionView(session) {
+	switch (session.state) {
+    case 'NEW':
+      return '\nWelcome to the Telnet server!\n\n'
+    case 'NEEDS_USERNAME':
+      return 'Username: '
+    case 'NEEDS_PASSWORD':
+      return 'Password: '
+    case 'AT_PLAY':
+      return config.prompt
+  }
+}
+
+module.exports = sessionView
