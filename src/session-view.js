addd014a598cd30b953785f0fd3fa57545ae0e78
Isaac Helbling <imhelbling@gmail.com>
2017-06-12 01:54:10 -0400

    whitespace

--- src/session-view.js
+++ src/session-view.js
@@ -2,14 +2,19 @@ const config = require('./config')
 
 function sessionView(session) {
 	switch (session.state) {
+
     case 'NEW':
       return '\nWelcome to the Telnet server!\n\n'
+
     case 'NEEDS_USERNAME':
       return 'Username: '
+
     case 'NEEDS_PASSWORD':
       return 'Password: '
+
     case 'AT_PLAY':
       return config.prompt
+      
   }
 }
 
